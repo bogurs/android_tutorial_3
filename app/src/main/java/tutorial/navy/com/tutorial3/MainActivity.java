4bbb8e5ba79d75c704a936755f2ee51da71957de
bogurs <thsong10@gmail.com>
2018-12-27 22:40:54 +0900

    activity_main.xml 메인 뷰 수정 MainActivity.java 메인 로직 수정

--- app/src/main/java/tutorial/navy/com/tutorial3/MainActivity.java
+++ app/src/main/java/tutorial/navy/com/tutorial3/MainActivity.java
@@ -2,6 +2,8 @@ package tutorial.navy.com.tutorial3;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.*;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        String[] items = {"망고 쥬스", "토마토 쥬스", "포토 쥬스"};
+        ListAdapter adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, items);
+        ListView listView = findViewById(R.id.listView);
+        listView.setAdapter(adapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String item = String.valueOf(parent.getItemAtPosition(position));
+                Toast.makeText(MainActivity.this, item, Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
